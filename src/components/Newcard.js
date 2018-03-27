dc73d506929d2ceff3b9353d45341962775c2e8e
Andreya <avalabek@yahoo.com>
2018-03-27 08:50:42 -0700

    cards line up in rows

--- src/components/Newcard.js
+++ src/components/Newcard.js
@@ -2,18 +2,24 @@ import React from "react";
 
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 const Newcard = props => (
-  
+  //if you need state changes use constructor 
   <wrapper>
-    {props.friends.map(item => (
-      <div className="card">
-        <img className="card-img-top" src={item.image} alt={item.name} />
-        <div className="card-body">
-          <h5 className="card-title" key={item.id}>
-        {item.name}
-          </h5>
+    <div className="row">
+        {props.friends.map(item => (
+    
+      <div className="col-md-4">
+        <div className="card">
+          <img className="card-img-top" src={item.image} alt={item.name} />
+            <div className="card-body">
+              <h5 className="card-title" key={item.id}>
+                {item.name}
+              </h5>
+            </div>
         </div>
-       </div>
-    ))}
+      </div>
+   
+        ))}
+     </div> 
   </wrapper>
   
 );
